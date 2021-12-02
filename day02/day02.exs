ef789a64f20bffbe160615a856d0ef5d01c7cd78
Tyler Wymer <twymer@gmail.com>
2021-12-02 15:39:50 +0100

    Refactor to use functions instead of cond

--- day02/day02.exs
+++ day02/day02.exs
@@ -49,7 +49,33 @@ defmodule Day02 do
     |> Enum.product
     |> IO.inspect
   end
+
+  def drive("up", angle, acc) do
+    %{aim: acc.aim - angle, horizontal: acc.horizontal, depth: acc.depth}
+  end
+  def drive("down", angle, acc) do
+    %{aim: acc.aim + angle, horizontal: acc.horizontal, depth: acc.depth}
+  end
+  def drive("forward", distance, acc) do
+    %{aim: acc.aim, horizontal: acc.horizontal + distance, depth: acc.depth + acc.aim * distance}
+  end
+
+  def star2_refactored() do
+    {:ok, contents} = File.read("day02.txt")
+
+    contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row -> String.split(row) end)
+    |> Enum.map(fn [command, distance] -> [command, String.to_integer(distance)] end)
+    |> Enum.reduce(%{aim: 0, horizontal: 0, depth: 0}, fn [command, distance], acc -> drive(command, distance, acc) end)
+    |> IO.inspect
+    |> Map.drop([:aim])
+    |> Map.values
+    |> Enum.product
+    |> IO.inspect
+  end
 end
 
 # Day02.star1()
-Day02.star2()
+# Day02.star2()
+Day02.star2_refactored()

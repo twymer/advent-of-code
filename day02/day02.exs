e59c000d28866f3626055024998fdb091d4ba0f5
Tyler Wymer <twymer@gmail.com>
2021-12-02 08:37:19 +0100

    Day 2 **

--- day02/day02.exs
+++ day02/day02.exs
@@ -1,4 +1,4 @@
-defmodule Day01 do
+defmodule Day02 do
   # STAR 1
 
   def star1() do
@@ -20,7 +20,32 @@ defmodule Day01 do
     end)
     |> IO.inspect
   end
+
+  # STAR 2
+
+  def star2() do
+    {:ok, contents} = File.read("day02.txt")
+
+    contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row -> String.split(row) end)
+    |> Enum.map(fn [command, distance] -> [command, String.to_integer(distance)] end)
+    |> Enum.reduce(%{aim: 0, horizontal: 0, depth: 0}, fn [command, distance], acc ->
+      case command do
+        "up" ->
+          %{aim: acc.aim - distance, horizontal: acc.horizontal, depth: acc.depth}
+        "down" ->
+          %{aim: acc.aim + distance, horizontal: acc.horizontal, depth: acc.depth}
+        "forward" ->
+          %{aim: acc.aim, horizontal: acc.horizontal + distance, depth: acc.depth + acc.aim * distance}
+      end
+    end)
+    |> IO.inspect
+  end
 end
 
-result = Day01.star1()
+# result = Day02.star1()
+# IO.inspect(result.horizontal * result.depth)
+
+result = Day02.star2()
 IO.inspect(result.horizontal * result.depth)

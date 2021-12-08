efbff54da2caa9988ec2ee51180d5cd574381725
Tyler Wymer <twymer@gmail.com>
2021-12-08 14:43:21 +0100

    Day 8 **

---
+++ 2021/day08/day08.exs
@@ -0,0 +1,135 @@
+defmodule Day08 do
+  def star1() do
+    File.read!("day08.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn row, acc ->
+      row_count = row
+                  |> String.split("|")
+                  |> Enum.map(&String.split/1)
+                  |> Enum.at(1)
+                  |> Enum.filter(&Enum.member?([2, 3, 4, 7], String.length(&1)))
+                  |> Enum.count
+
+      acc + row_count
+    end)
+    |> IO.inspect
+  end
+
+  def map_for_digits(inputs) do
+    # Order matters here, since some need to be discovered first
+    numbers = [1, 4, 7, 8, 0, 6, 2, 3, 5, 9]
+
+    numbers
+    |> Enum.reduce(%{}, fn number, acc ->
+      signals =
+        case number do
+          1 ->
+            inputs
+            |> Enum.find(&(Enum.count(&1) === 2))
+          4 ->
+            inputs
+            |> Enum.find(&(Enum.count(&1) === 4))
+          7 ->
+            inputs
+            |> Enum.find(&(Enum.count(&1) === 3))
+          8 ->
+            inputs
+            |> Enum.find(&(Enum.count(&1) === 7))
+          0 ->
+            # Six elements, and has 3 pieces from the signal for 4 and all of 7
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 6 &&
+                Enum.count(acc[4] -- signal) === 1 &&
+                Enum.count(acc[7] -- signal) === 0
+            end)
+          2 ->
+            # Signal for 6, missing two, distinguished from 3 by missing one from signals for 1
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 5 &&
+                Enum.count(acc[6] -- signal) === 2 &&
+                Enum.count(acc[1] -- signal) === 1
+            end)
+          5 ->
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 5 && Enum.count(acc[6] -- signal) === 1
+            end)
+          3 ->
+            # Five elements and contains all of the signal for 1
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 5 && Enum.count(acc[1] -- signal) === 0
+            end)
+          6 ->
+            # Six elements and contains only one from the signal for 1
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 6 && Enum.count(acc[1] -- signal) === 1
+            end)
+          9 ->
+           # Six elements and contains all of signals for 4 and 7
+            inputs
+            |> Enum.find(fn signal ->
+              Enum.count(signal) === 6 &&
+                Enum.count(acc[4] -- signal) === 0 &&
+                Enum.count(acc[7] -- signal) === 0
+            end)
+        end
+
+      Map.put(acc, number, signals)
+    end)
+  end
+
+  def calculate_row_value(row) do
+    [inputs, outputs] =
+      row
+      |> String.split("|")
+      |> Enum.map(&String.split/1)
+
+    inputs =
+      inputs
+      |> Enum.map(&(String.split(&1, "", trim: true)))
+
+    decoder =
+      inputs
+      |> map_for_digits
+      |> Map.new(fn {k, v} ->
+        {
+          v
+          |> Enum.sort
+          |> Enum.join(""),
+          k
+        }
+      end)
+
+    outputs
+    |> Enum.reduce([], fn output, acc ->
+      output_key =
+        output
+        |> String.split("", trim: true)
+        |> Enum.sort
+        |> Enum.join("")
+
+      acc ++ [Map.get(decoder, output_key)]
+    end)
+    |> Enum.join("")
+    |> String.to_integer
+  end
+
+  def star2() do
+    File.read!("day08.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&calculate_row_value/1)
+    |> IO.inspect
+    |> Enum.sum
+    |> IO.inspect
+  end
+
+  def all do
+    @all_signals
+  end
+end
+
+Day08.star2()

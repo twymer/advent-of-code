5037025d916ed967ed4ab06c9d445d067f72b2a9
Tyler Wymer <twymer@gmail.com>
2021-12-20 21:31:09 +0100

    Refactor to not depend on hacks (before looking at how others handled the infinite space)

--- 2021/day20/day20.exs
+++ 2021/day20/day20.exs
@@ -1,49 +1,37 @@
 defmodule Day20 do
   def max_index(:x, grid) do
-    250
-    # grid
-    # |> Enum.filter(fn {_, v} -> v === "#" end)
-    # |> Map.new
-    # |> Map.keys
-    # |> Enum.map(&(&1 |> Kernel.elem(0)))
-    # |> Enum.max
+    grid
+    |> Map.keys
+    |> Enum.map(&(&1 |> Kernel.elem(0)))
+    |> Enum.max
   end
 
   def max_index(:y, grid) do
-    250
-    # grid
-    # |> Enum.filter(fn {_, v} -> v === "#" end)
-    # |> Map.new
-    # |> Map.keys
-    # |> Enum.map(&(&1 |> Kernel.elem(1)))
-    # |> Enum.max
+    grid
+    |> Map.keys
+    |> Enum.map(&(&1 |> Kernel.elem(1)))
+    |> Enum.max
   end
 
   def min_index(:x, grid) do
-    -100
-    # grid
-    # |> Enum.filter(fn {_, v} -> v === "#" end)
-    # |> Map.new
-    # |> Map.keys
-    # |> Enum.map(&(&1 |> Kernel.elem(0)))
-    # |> Enum.min
+    grid
+    |> Map.keys
+    |> Enum.map(&(&1 |> Kernel.elem(0)))
+    |> Enum.min
   end
 
   def min_index(:y, grid) do
-    -20
-    # grid
-    # |> Enum.filter(fn {_, v} -> v === "#" end)
-    # |> Map.new
-    # |> Map.keys
-    # |> Enum.map(&(&1 |> Kernel.elem(1)))
-    # |> Enum.min
+    grid
+    |> Map.keys
+    |> Enum.map(&(&1 |> Kernel.elem(1)))
+    |> Enum.min
   end
 
   def print_grid(grid) do
     IO.puts("")
-    Enum.each((min_index(:y, grid) - 20)..(max_index(:y, grid) + 20), fn row ->
-      Enum.reduce((min_index(:x, grid) - 20)..(max_index(:x, grid) + 20), [], fn column, acc ->
-        acc ++ [Map.get(grid, {column, row}, ".")]
+    Enum.each((min_index(:y, grid) - 5)..(max_index(:y, grid) + 5), fn row ->
+      Enum.reduce((min_index(:x, grid) - 5)..(max_index(:x, grid) + 5), [], fn column, acc ->
+        acc ++ [Map.get(grid, {column, row}, " ")]
       end)
       |> Enum.join("")
       |> IO.puts
@@ -87,31 +75,42 @@ defmodule Day20 do
     {input_image, enhancement_algo}
   end
 
-  def convert_pixel(input_image, enhancement_algo, {x, y}) do
-    foo = Enum.reduce(-1..1, [], fn j, acc ->
+  def convert_pixel(input_image, enhancement_algo, {x, y}, step) do
+    Enum.reduce(-1..1, "", fn j, acc ->
       Enum.reduce(-1..1, acc, fn i, acc ->
-        # IO.puts "x: #{x+i}, y: #{y+j}, hit: #{Map.get(input_image, {x+i, y+j})}"
-        if Map.get(input_image, {x+i, y+j}, ".")  === "#" do
-          acc ++ [1]
+        if Map.get(input_image, {x+i, y+j}, get_infinity(enhancement_algo, step)) === "#" do
+          acc <> "1"
         else
-          acc ++ [0]
+          acc <> "0"
         end
       end)
     end)
-    |> Enum.join
     |> String.to_integer(2)
     |> then(&(Map.get(enhancement_algo, &1)))
   end
 
-  def enhance(input_image, enhancement_algo) do
-    offset = 0
+  def get_infinity(enhancement_algo, step) do
+    # NOTE this encodes some bias that infinity starts out as unlit pixels.
+    # If the start state had lit pixels stretching outwards this would break.
+    cond do
+      Map.get(enhancement_algo, 0) == "." ->
+        "."
+      rem(step, 2) == 1 ->
+        Map.get(enhancement_algo, 0)
+      true ->
+        Map.get(enhancement_algo, 511)
+    end
+  end
+
+  def enhance(input_image, enhancement_algo, step) do
+    offset = 1
     x_range = (min_index(:x, input_image) - offset)..(max_index(:x, input_image) + offset)
     y_range = (min_index(:y, input_image) - offset)..(max_index(:y, input_image) + offset)
 
     Enum.reduce(y_range, %{}, fn y, acc ->
       Enum.reduce(x_range, acc, fn x, acc ->
         input_image
-        |> convert_pixel(enhancement_algo, {x, y})
+        |> convert_pixel(enhancement_algo, {x, y}, step)
         |> then(&(Map.put(acc, {x, y}, &1)))
       end)
     end)
@@ -120,23 +119,36 @@ defmodule Day20 do
   def star1 do
     {input_image, enhancement_algo} = load_file()
 
-    0..49
+    0..2
     |> Enum.reduce(input_image, fn step, acc ->
-      IO.puts "step #{step}"
+      IO.puts "Step #{step}"
+
       acc
-      |> enhance(enhancement_algo)
+      |> enhance(enhancement_algo, step)
     end)
     |> print_grid
-    |> Enum.filter(fn {{x, y}, _} -> x in -45..200 && y in -110..200 end)
-    |> Map.new
+    |> Map.values
+    |> Enum.frequencies
+    |> Map.get("#")
+    |> IO.inspect
+  end
+
+  def star2 do
+    {input_image, enhancement_algo} = load_file()
+
+    0..49
+    |> Enum.reduce(input_image, fn step, acc ->
+      IO.puts "Step #{step}"
+
+      acc
+      |> enhance(enhancement_algo, step)
+    end)
     |> print_grid
     |> Map.values
     |> Enum.frequencies
-    |> IO.inspect
-    |> Map.values
-    |> Enum.frequencies
+    |> Map.get("#")
     |> IO.inspect
   end
 end
 
-Day20.star1
+Day20.star2()

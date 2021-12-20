87d2ed17a881651feffcbe149dd40c20aec3723f
Tyler Wymer <twymer@gmail.com>
2021-12-20 17:51:40 +0100

    Super hacky star 2

--- 2021/day20/day20.exs
+++ 2021/day20/day20.exs
@@ -1,38 +1,42 @@
 defmodule Day20 do
   def max_index(:x, grid) do
-    grid
-    |> Enum.filter(fn {_, v} -> v === "#" end)
-    |> Map.new
-    |> Map.keys
-    |> Enum.map(&(&1 |> Kernel.elem(0)))
-    |> Enum.max
+    250
+    # grid
+    # |> Enum.filter(fn {_, v} -> v === "#" end)
+    # |> Map.new
+    # |> Map.keys
+    # |> Enum.map(&(&1 |> Kernel.elem(0)))
+    # |> Enum.max
   end
 
   def max_index(:y, grid) do
-    grid
-    |> Enum.filter(fn {_, v} -> v === "#" end)
-    |> Map.new
-    |> Map.keys
-    |> Enum.map(&(&1 |> Kernel.elem(1)))
-    |> Enum.max
+    250
+    # grid
+    # |> Enum.filter(fn {_, v} -> v === "#" end)
+    # |> Map.new
+    # |> Map.keys
+    # |> Enum.map(&(&1 |> Kernel.elem(1)))
+    # |> Enum.max
   end
 
   def min_index(:x, grid) do
-    grid
-    |> Enum.filter(fn {_, v} -> v === "#" end)
-    |> Map.new
-    |> Map.keys
-    |> Enum.map(&(&1 |> Kernel.elem(0)))
-    |> Enum.min
+    -100
+    # grid
+    # |> Enum.filter(fn {_, v} -> v === "#" end)
+    # |> Map.new
+    # |> Map.keys
+    # |> Enum.map(&(&1 |> Kernel.elem(0)))
+    # |> Enum.min
   end
 
   def min_index(:y, grid) do
-    grid
-    |> Enum.filter(fn {_, v} -> v === "#" end)
-    |> Map.new
-    |> Map.keys
-    |> Enum.map(&(&1 |> Kernel.elem(1)))
-    |> Enum.min
+    -20
+    # grid
+    # |> Enum.filter(fn {_, v} -> v === "#" end)
+    # |> Map.new
+    # |> Map.keys
+    # |> Enum.map(&(&1 |> Kernel.elem(1)))
+    # |> Enum.min
   end
 
   def print_grid(grid) do
@@ -100,7 +104,7 @@ defmodule Day20 do
   end
 
   def enhance(input_image, enhancement_algo) do
-    offset = 10
+    offset = 0
     x_range = (min_index(:x, input_image) - offset)..(max_index(:x, input_image) + offset)
     y_range = (min_index(:y, input_image) - offset)..(max_index(:y, input_image) + offset)
 
@@ -116,21 +120,22 @@ defmodule Day20 do
   def star1 do
     {input_image, enhancement_algo} = load_file()
 
-    input_image
-    # |> print_grid
-    |> enhance(enhancement_algo)
+    0..49
+    |> Enum.reduce(input_image, fn step, acc ->
+      IO.puts "step #{step}"
+      acc
+      |> enhance(enhancement_algo)
+    end)
     |> print_grid
-    |> enhance(enhancement_algo)
-    |> print_grid
-    |> Enum.filter(fn {{x, y}, _} -> x in -9..108 && y in -10..108 end)
+    |> Enum.filter(fn {{x, y}, _} -> x in -45..200 && y in -110..200 end)
     |> Map.new
     |> print_grid
     |> Map.values
     |> Enum.frequencies
     |> IO.inspect
-
-    # convert_pixel(input_image, enhancement_algo, {2, 2})
-    # |> print_grid
+    |> Map.values
+    |> Enum.frequencies
+    |> IO.inspect
   end
 end
 

d604ef4ac93c68bbc0b99b9bbdf9565280884ba9
Tyler Wymer <twymer@gmail.com>
2022-12-02 09:16:05 -0600

    Rename variables

--- 2022/day02/day.rb
+++ 2022/day02/day.rb
@@ -9,10 +9,10 @@ def score_round(round)
   opponent_index = choices.index(round[0])
   result = results.index(round[1])
 
-  chosen_move_index = (opponent_index + result - 1) % 3 + 1
-  result_scoring = results.index(round[1]) * 3
+  chosen_move_score = (opponent_index + result - 1) % 3 + 1
+  result_score = results.index(round[1]) * 3
 
-  chosen_move_index + result_scoring
+  chosen_move_score + result_score
 end
 
 puts load_file.reduce(0) { |sum, round| sum + score_round(round) }

d4828094415d52e9ba57ddc0c0284982a761ed67
Tyler Wymer <twymer@gmail.com>
2022-12-04 10:00:05 -0600

    Refactor

--- 2022/day04/day.rb
+++ 2022/day04/day.rb
@@ -1,23 +1,14 @@
 def load_file(filename = 'input.txt')
   File.open(filename).readlines.map do |line|
     line.chomp.split(',').map do |assignments|
-      assignments.split('-').map(&:to_i)
+      Range.new(*assignments.split('-').map(&:to_i))
     end
   end
 end
 
 def count_overlaps(pair_assignments)
   pair_assignments.count do |elf_one, elf_two|
-    (
-      (
-        elf_one.first.between?(elf_two.first, elf_two.last) ||
-        elf_one.last.between?(elf_two.first, elf_two.last)
-      ) ||
-      (
-        elf_two.first.between?(elf_one.first, elf_one.last) ||
-        elf_two.last.between?(elf_one.first, elf_one.last)
-      )
-    )
+    elf_one.cover?(elf_two.first) || elf_two.cover?(elf_one.first)
   end
 end
 

d5995cbd4be7fdd0a4455eb22d235818969a18c7
Tyler Wymer <twymer@gmail.com>
2022-12-05 12:19:31 -0600

    Cleanup

--- 2022/day05/day.rb
+++ 2022/day05/day.rb
@@ -1,30 +1,27 @@
 def load_file(filename = 'input.txt')
-  stacks = {}
+  stacks = Hash.new { |h, k| h[k] = [] }
   instructions = []
+
   File.open(filename).readlines.each do |line|
     if line.include? '['
-      crates = line.chars.each_slice(4).to_a.map { |x| x.join.delete("[] \n") }
+      crates = line.chars.each_slice(4).map { |x| x.join.delete("[] \n") }
+
       crates.each_with_index do |crate, i|
-        unless crate.empty?
-          if stacks.has_key?(i + 1)
-            stacks[i + 1] << crate
-          else
-            stacks[i + 1] = [crate]
-          end
-        end
+        next if crate.empty?
+        stacks[i + 1] << crate
       end
-    elsif
-      line.include? 'move'
+    elsif line.include? 'move'
       instructions << line.chomp
     end
   end
+
   [stacks, instructions]
 end
 
 def run_instructions(stacks, instructions)
   instructions.each do |instruction|
     count, from, to = instruction.scan(/(\d+)/).flatten.map(&:to_i)
-    stacks[to].unshift(*stacks[from].shift(count.to_i))
+    stacks[to].unshift(*stacks[from].shift(count))
   end
   stacks
 end
@@ -33,6 +30,6 @@ def get_answer(stacks)
   stacks.sort.map { |k, v| v.first }.join
 end
 
-stacks, instructions = load_file #('example.txt')
+stacks, instructions = load_file
 result_stacks = run_instructions(stacks, instructions)
 puts get_answer(result_stacks)

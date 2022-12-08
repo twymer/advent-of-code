fc18e2255a7caa97e7b6d31f0d4a9900f311a032
Tyler Wymer <twymer@gmail.com>
2022-12-08 13:05:18 -0600

    Day 8 **

--- 2022/day08/day.rb
+++ 2022/day08/day.rb
@@ -4,6 +4,7 @@ def load_file(filename = 'input.txt')
   end
 end
 
+# Part 1
 def is_visible?(forest, position)
   height = forest.count
   width = forest[0].count
@@ -17,7 +18,8 @@ def is_visible?(forest, position)
   )
 end
 
-def count_visible_trees(forest)
+# Part 1
+def count_visible_from_edge(forest)
   forest.each_with_index.reduce(0) do |count, (row, j)|
     count + row.each_with_index.reduce(0) do |inner_count, (tree, i)|
       inner_count + (is_visible?(forest, [i, j]) ? 1 : 0)
@@ -25,5 +27,33 @@ def count_visible_trees(forest)
   end
 end
 
-forest = load_file('example.txt')
-puts count_visible_trees(forest)
+# Part 2
+def count_visible(forest, position)
+  height = forest.count
+  width = forest[0].count
+  tree = forest[position[1]][position[0]]
+
+  [
+    # top
+    (position[1] - 1).downto(0).reduce(0) { |count, j| forest[j][position[0]] >= tree ? (break count + 1) : count + 1 },
+    # bottom
+    (position[1] + 1).upto(height - 1).reduce(0) { |count, j| forest[j][position[0]] >= tree ? (break count + 1) : count + 1 },
+    # left
+    (position[0] - 1).downto(0).reduce(0) { |count, i| forest[position[1]][i] >= tree ? (break count + 1) : count + 1 },
+    # right
+    (position[0] + 1).upto(height - 1).reduce(0) { |count, i| forest[position[1]][i] >= tree ? (break count + 1) : count + 1 }
+  ].inject(:*)
+end
+
+# Part 2
+def calculate_scenic_scores(forest)
+  forest.count.times.map do |j|
+    forest[0].count.times.map do |i|
+      count_visible(forest, [i, j])
+    end
+  end
+end
+
+forest = load_file
+puts count_visible_from_edge(forest)
+puts calculate_scenic_scores(forest).flatten.max

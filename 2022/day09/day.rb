f81a8af3edb44dfe3f4a873a30bfa76f958f3d2d
Tyler Wymer <twymer@gmail.com>
2022-12-09 10:58:04 -0600

    Day 9 **

--- 2022/day09/day.rb
+++ 2022/day09/day.rb
@@ -19,21 +19,25 @@ def diff_vectors(left, right)
   left.zip(right).map { |l, r| l - r }
 end
 
-def follow_instruction(head, tail, dir, dist)
-  return [head, tail, [tail]] if dist == 0
+def follow_instruction(head, knots, dir, dist)
+  return [head, knots, [knots.last]] if dist == 0
 
   head = add_vectors(head, DIRS[dir])
-  diff = diff_vectors(head, tail)
-  tail = add_vectors(tail, diff.map { |p| p <=> 0 }) if diff[0].abs > 1 || diff[1].abs > 1
 
-  follow_instruction(head, tail, dir, dist - 1).yield_self do |h, t, v|
-    [h, t, v << tail]
+  moved_knots = knots.each_with_index.reduce(knots.dup) do |acc_knots, (knot, i)|
+    diff = i == 0 ? diff_vectors(head, knot) : diff_vectors(acc_knots[i - 1], knot)
+    acc_knots[i] = add_vectors(knot, diff.map { |p| p <=> 0 }) if diff[0].abs > 1 || diff[1].abs > 1
+    acc_knots
+  end
+
+  follow_instruction(head, moved_knots, dir, dist - 1).yield_self do |h, k, v|
+    [h, k, v << moved_knots.last]
   end
 end
 
-instructions = load_file #('example.txt')
-final_state = instructions.reduce([[0, 0], [0, 0], []]) do |acc, (dir, dist)|
-  head, tail, visited = follow_instruction(acc[0], acc[1], dir, dist)
-  [head, tail, acc[2] + visited]
+instructions = load_file
+final_state = instructions.reduce([[0, 0], Array.new(9) { [0, 0] }, []]) do |acc, (dir, dist)|
+  head, knots, visited = follow_instruction(acc[0], acc[1], dir, dist)
+  [head, knots, acc[2] + visited]
 end
 puts final_state[2].uniq.count
